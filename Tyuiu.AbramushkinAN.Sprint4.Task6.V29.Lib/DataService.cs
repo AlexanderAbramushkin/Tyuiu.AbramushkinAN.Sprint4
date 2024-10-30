9a80ce676ad8d6fd996eb711d27ccbdbc9e48f5f
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2024-10-30 21:17:35 +0500

    Task6

---
+++ Tyuiu.AbramushkinAN.Sprint4.Task6.V29.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.AbramushkinAN.Sprint4.Task6.V29.Lib
+{
+    public class DataService : ISprint4Task6V29
+    {
+        public int Calculate(string[] array)
+        {
+            int count = array.Count(len => len.Length > 4);
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.AbramushkinAN.Sprint4.Task6.V29/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.AbramushkinAN.Sprint4.Task6.V29.Lib;
+
+namespace Tyuiu.AbramushkinAN.Sprint4.Task6.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            string[] array = new string[]{ "Весна", "Лето", "Осень", "Зима" };
+
+            Console.Title = "Спринт #4 | Выполнил: Абрамушкин А. Н. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Абрамушкин Александр Николаевич | ИИПб-24-2                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных [\"Весна\", \"Лето\", \"Осень\", \"Зима\"]          *");
+            Console.WriteLine("* используя класс Array подсчитайте количество элементов, длина которых   *");
+            Console.WriteLine("* больше 4.                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"\"{array[i]}\"  ");
+            }
+            Console.WriteLine();
+
+
+            int Result = ds.Calculate(array);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($" Количество элементов, длина которых больше 4 = {Result}");
+        }
+    }
+}
